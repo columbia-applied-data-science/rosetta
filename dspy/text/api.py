9b99ba4edabd45eac1047233b5437b52d1ed88c7
Ian Langmore <ianlangmore@gmail.com>
2013-11-07 09:34:02 -0500

    UP: Added LDAResults to text.api

--- dspy/text/api.py
+++ dspy/text/api.py
@@ -1,3 +1,6 @@
 from dspy.text.streamers import TextFileStreamer
+
 from dspy.text.text_processors import \
     TokenizerBasic, MakeTokenizer, SFileFilter, VWFormatter
+
+from dspy.text.vw_helpers import LDAResults
